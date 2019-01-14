fe71f13b6d17bd0263f6ca31e2b3ca0de68db486
unknown <sabouretmaxime@gmail.com>
2019-01-14 12:49:17 +0100

    slider with parameters

--- js/sliderclass.js
+++ js/sliderclass.js
@@ -1,181 +1,224 @@
 class Slider {
 
     constructor() {
-        this.next = null;
-        this.prev = null;
+        this.slider = null;
+        this.imgs = null;
+        this.displayNextBeforeButtons = null;
+        this.controlButtonsStartStop = null;
+
+        // not initialized in the init because i use init only if controlButtonsStartStop is on true, good method ?
         this.stopBtn = null;
         this.restartBtn = null;
-        this.btnFirstSlide = null;
+
         this.currentSlide = null;
         this.run = null;
         this.widthBar = null;
         this.intvl = null;
-        this.intervals = []; /* Will be used to store the intervals in a global table and to be able to delete them */
-        this.imagesSlide = ["imgSlid1", "imgSlid2", "imgSlid3", "imgSlid4"];
+        this.intervals = []; // Will be used to store the intervals in a global table and to be able to delete them
     }
 
-    init() {
-        this.next = document.getElementById("next");
-        this.prev = document.getElementById("prev");
-        this.stopBtn = document.getElementById("stopDiap");
-        this.restartBtn = document.getElementById("startDiap");
-        this.btnFirstSlide = document.getElementById("firstButtonSlide");
+    init(idSlider, slider, imgs, displayNextBeforeButtons, controlButtonsStartStop, displayProgressBar) {
+        /*
+        this.btnFirstSlide = document.getElementById("firstButtonSlide");*/
+
+        this.idSlider = idSlider;
+        this.slider = slider;
+        this.imgs = imgs;
+        this.displayNextBeforeButtons = displayNextBeforeButtons;
+        this.controlButtonsStartStop = controlButtonsStartStop;
+        this.displayProgressBar = displayProgressBar;
         this.currentSlide = 1;
-        this.run = 1; /* for the spacebar event, to know if the progressBar runs */
+        this.run = true; /* for the spacebar event, to know if the progressBar runs */
         this.widthBar = 0;
     }
 
     createHtml() {
-        let sectionId = document.getElementById("diaporama");
 
-        /* CENTER */
-        let divCenterText = document.createElement("div");
-        divCenterText.classList.add("row", "centerText");
-        sectionId.appendChild(divCenterText);
+        let sectionId = document.getElementById(this.idSlider);
 
-        /* IMAGES */
-        this.imagesSlide.forEach(function (element) {
-            let divImg = document.createElement("div");
-            divImg.classList.add("imgDiap");
-            divImg.setAttribute("id", element);
+        // Create a div to contain the slider, necessary ?
+        let sliderContainer = document.createElement("div");
+        sliderContainer.classList.add("row", "relativePos");
+        sectionId.appendChild(sliderContainer);
 
-            let divBlockInstru = document.createElement("div");
-            divBlockInstru.classList.add("blockInstructions");
+        this.createImages(sliderContainer);
+        this.createInterface(sliderContainer);
+        this.createEvents();
+    }
 
-            let spanInstruction = document.createElement("span");
-            spanInstruction.classList.add("instructionsWhite");
+    createImages(sliderContainer) {
 
-            if (element == "imgSlid1") {
+        this.imgs.forEach(function (elementImage, index) {
+            // IMAGE
+            let figure = document.createElement("figure");
+            figure.classList.add("figureDiap");
+            figure.setAttribute("id", 'fig' + index);
 
-                let titleImg = document.createElement("h1");
-                titleImg.classList.add("titleDiap");
+            let image = document.createElement("img");
+            image.setAttribute('src', elementImage.url);
+            image.setAttribute('alt', elementImage.alt);
+            image.setAttribute('title', elementImage.title);
+            image.style.height = '720px';
+            image.style.width = '100%';
 
-                let lobsterStyleSMF = document.createElement("span");
-                lobsterStyleSMF.classList.add("lobsterStyle");
-                lobsterStyleSMF.textContent = "SMF Vélos";
-                titleImg.appendChild(lobsterStyleSMF);
+            // FIGCAPTION
+            let figcaption = document.createElement("figcaption");
+            figcaption.classList.add("blockFigcaption");
 
-                titleImg.append(" , l'application de réservation "); /* allow to add object or text after the last child */
+            let figcaptionTitle = document.createElement("div");
+            figcaptionTitle.classList.add("titleDiap");
+            figcaptionTitle.textContent = elementImage.figcaptionTitle;
 
-                let spanGreen = document.createElement("span");
-                spanGreen.classList.add("vertT");
-                spanGreen.textContent = "écolo !";
-                titleImg.appendChild(spanGreen);
+            let figcaptionDescription = document.createElement("div");
+            if (index === 0) {
+                figcaptionDescription.classList.add("instructions");
+            } else {
+                figcaptionDescription.classList.add("instructionsWhite");
+            }
+            figcaptionDescription.textContent = elementImage.figcaptionText;
 
-                spanInstruction.textContent = `Ce diaporama vous explique toute la démarche à suivre pour réserver en ligne, 
-                alors n'hésitez plus !`;
-                spanInstruction.classList.remove("instructionsWhite");
-                spanInstruction.classList.add("instructions");
-
-                let btnFirstSlide = document.createElement("a");
-                btnFirstSlide.classList.add("btn", "btn-primary");
-                btnFirstSlide.setAttribute("id","firstButtonSlide")
-                btnFirstSlide.textContent = "Cliquez-ici pour en savoir plus";
-
-                divBlockInstru.appendChild(titleImg);
-                divBlockInstru.appendChild(spanInstruction);
-                divBlockInstru.appendChild(btnFirstSlide);
-
-
-            } else if (element == "imgSlid2") {
-
-                spanInstruction.textContent = `C'est simple et rapide ! La carte vous permet de choisir la station la
-                plus proche, il suffit de cliquer dessus`;
-                divBlockInstru.appendChild(spanInstruction);
-
-            } else if (element == "imgSlid3") {
-
-                spanInstruction.textContent = `Des informations vous sont données concernant le nombre de
-                disponibilités`;
-                divBlockInstru.appendChild(spanInstruction);
-
-            } else if (element == "imgSlid4") {
-
-                spanInstruction.textContent = `Vous signez et c'est terminé !`;
-                divBlockInstru.appendChild(spanInstruction);
-
+            // DELETE DISPLAY FOR TEST
+            if (index !== 0) {
+                figure.style.display = 'none';
             }
 
-            divImg.appendChild(divBlockInstru);
-            divCenterText.appendChild(divImg);
+            figcaption.appendChild(figcaptionTitle);
+            figcaption.appendChild(figcaptionDescription);
+            figure.appendChild(image);
+            figure.appendChild(figcaption);
+            sliderContainer.appendChild(figure);
         });
 
-        /* BUTTONS */
+    }
 
-        let prev = document.createElement("a");
-        prev.setAttribute("id", "prev");
-        prev.innerHTML = "&#10094;";
+    createInterface(sliderContainer) {
 
-        let next = document.createElement("a");
-        next.setAttribute("id", "next");
-        next.innerHTML = "&#10095;";
+        // BUTTONS PREV AND NEXT
+        if (this.displayNextBeforeButtons === true) {
 
-        divCenterText.appendChild(prev);
-        divCenterText.appendChild(next);
+            let prev = document.createElement("a");
+            prev.setAttribute("id", "prev");
+            prev.innerHTML = "&#10094;";
 
-        /* CONTROL */
+            let next = document.createElement("a");
+            next.setAttribute("id", "next");
+            next.innerHTML = "&#10095;";
 
-        let divContainControl = document.createElement("div");
-        divContainControl.classList.add("container-fluid");
+            sliderContainer.appendChild(prev);
+            sliderContainer.appendChild(next);
+        }
 
-        let divControl = document.createElement("div");
-        divControl.classList.add("col-12", "row", "controlDiap");
+        // CONTROL START AND STOP
+        if (this.controlButtonsStartStop === true) {
 
-        let startContain = document.createElement("div");
-        startContain.classList.add("offset-5", "col-1");
-        let restartBtn = document.createElement("a");
-        restartBtn.setAttribute("id", "startDiap");
-        let icoStart = document.createElement("i");
-        icoStart.classList.add("fas", "fa-play");
-        restartBtn.appendChild(icoStart);
-        startContain.appendChild(restartBtn);
+            let divControl = document.createElement("div");
+            divControl.classList.add("col-12", "row", "controlDiap");
 
-        let stopContain = document.createElement("div");
-        stopContain.classList.add("col-1");
-        let stopBtn = document.createElement("a");
-        stopBtn.setAttribute("id", "stopDiap");
-        let icoStop = document.createElement("i");
-        icoStop.classList.add("fas", "fa-pause");
-        stopBtn.appendChild(icoStop);
-        stopContain.appendChild(stopBtn);
+            let startContain = document.createElement("div");
+            startContain.classList.add("offset-5", "col-1");
+            let restartBtn = document.createElement("a");
+            restartBtn.setAttribute("id", "startDiap");
+            let icoStart = document.createElement("i");
+            icoStart.classList.add("fas", "fa-play");
+            restartBtn.appendChild(icoStart);
+            startContain.appendChild(restartBtn);
 
-        divControl.appendChild(startContain);
-        divControl.appendChild(stopContain);
-        divContainControl.appendChild(divControl);
+            let stopContain = document.createElement("div");
+            stopContain.classList.add("col-1");
+            let stopBtn = document.createElement("a");
+            stopBtn.setAttribute("id", "stopDiap");
+            let icoStop = document.createElement("i");
+            icoStop.classList.add("fas", "fa-pause");
+            stopBtn.appendChild(icoStop);
+            stopContain.appendChild(stopBtn);
 
-        divCenterText.appendChild(divContainControl);
+            divControl.appendChild(startContain);
+            divControl.appendChild(stopContain);
+
+            sliderContainer.appendChild(divControl);
+
+            // GOOD TO PUT THAT HERE ?
+            this.stopBtn = document.getElementById("stopDiap");
+            this.restartBtn = document.getElementById("startDiap");
+        }
 
         /* PROGRESS BAR */
+        if (this.displayProgressBar === true) {
+            let divProgress = document.createElement("div");
+            divProgress.setAttribute("id", "myProgress");
 
-        let divProgress = document.createElement("div");
-        divProgress.setAttribute("id", "myProgress");
+            let divTime = document.createElement("div");
+            divTime.setAttribute("id", "divTime");
 
-        let divTime = document.createElement("div");
-        divTime.setAttribute("id", "divTime");
+            divProgress.appendChild(divTime);
+            sliderContainer.appendChild(divProgress);
+        }
+    }
 
-        divProgress.appendChild(divTime);
-        divCenterText.appendChild(divProgress);
+    createEvents() {
+
+        /*this.slider.btnFirstSlide.addEventListener("click", function (e) {
+            slider.changeSlide(1, true);
+        });*/
+
+        if (this.controlButtonsStartStop === true) {
+            this.stopBtn.addEventListener("click", function (e) {
+                slider.startOrStopProgressBar(false);
+            });
+
+            this.restartBtn.addEventListener("click", function (e) {
+                slider.startOrStopProgressBar(true);
+            });
+        }
+
+        if (this.displayNextBeforeButtons === true) {
+            let next = document.getElementById("next");
+            let prev = document.getElementById("prev");
+
+            next.addEventListener("click", function (e) {
+                slider.changeSlide(1, true);
+            });
+
+            prev.addEventListener("click", function (e) {
+                slider.changeSlide(-1, true);
+            });
+        }
+
+        document.addEventListener("keyup", function (e) {
+            if (e.keyCode == "37") {
+                slider.changeSlide(-1, true);
+            }
+            if (e.keyCode == "39") {
+                slider.changeSlide(1, true);
+            }
+            if (e.keyCode == "32" && run === true) {
+                //slider.startOrStopProgressBar(false);
+            } else if (e.keyCode == "32" && run === false) {
+                //slider.startOrStopProgressBar(true);
+            }
+
+        });
 
     }
 
     changeSlide(changeSlideVar, run) {
-        let numberOfSlides = document.getElementsByClassName("imgDiap");
+        let allSlides = document.getElementsByClassName("figureDiap");
         this.run = run;
 
         this.currentSlide = this.currentSlide + changeSlideVar;
 
         if (this.currentSlide <= 0) {
-            this.currentSlide = numberOfSlides.length;
+            this.currentSlide = allSlides.length;
         }
-        if (this.currentSlide > numberOfSlides.length) {
+        if (this.currentSlide > allSlides.length) {
             this.currentSlide = 1;
         }
 
-        for (let i = 0; i < numberOfSlides.length; i++) {
-            numberOfSlides[i].style.display = "none";
+        for (let i = 0; i < allSlides.length; i++) {
+            allSlides[i].style.display = "none";
         }
 
-        numberOfSlides[this.currentSlide - 1].style.display = "block";
+        allSlides[this.currentSlide - 1].style.display = "block";
 
         // Each time we change the slide, the progress bar is reinitialized
         this.progressBar(0);
@@ -191,66 +234,26 @@ class Slider {
     frame(widthBar2, elem) {
         if (widthBar2 >= 100) {
             clearInterval(this.intvl);
-            changeSlide(1, 1);
+            changeSlide(1, true);
             widthBar2 = 0;
             this.widthBar = widthBar2;
         } else {
             widthBar2 = widthBar2 + 0.4;
             elem.style.width = widthBar2 + '%';
-            this.widthBar = widthBar2; /* give the information globally notably for the startProgressBar function */
+            this.widthBar = widthBar2; // give the information globally notably for the startOrStopProgressBar function 
         }
     }
 
-    stopProgressBar(run) {
+    startOrStopProgressBar(run){
         this.run = run;
-        this.intervals.forEach(clearInterval); // clear all the intervals used if some exist, so it stop the progress bar
+        if(run === true){
+            this.progressBar(this.widthBar);
+        }else{
+            this.intervals.forEach(clearInterval);
+        }
+
     }
 
-    startProgressBar(run) {
-        this.run = run;
-        this.progressBar(this.widthBar); // clear all the intervals used if some exist, so it stops the progress bar
-    }
 
 }
-
-let slider = new Slider;
-slider.createHtml(); // Why can't i put this line after the eventListeners ??????
-slider.init(); // Why if i put that at the end the buttons don't work ?
-// We start the progress bar
-slider.progressBar(0);
-
-slider.btnFirstSlide.addEventListener("click", function (e) {
-    slider.changeSlide(1, 1);
-});
-
-slider.stopBtn.addEventListener("click", function (e) {
-    slider.stopProgressBar(0);
-});
-
-slider.restartBtn.addEventListener("click", function (e) {
-    slider.startProgressBar(1);
-});
-
-slider.next.addEventListener("click", function (e) {
-    slider.changeSlide(1, 1);
-});
-
-slider.prev.addEventListener("click", function (e) {
-    slider.changeSlide(-1, 1);
-});
-
-document.addEventListener("keyup", function (e) {
-    if (e.keyCode == "37") { /* Left Arrow */
-        slider.changeSlide(-1, 1);
-    }
-    if (e.keyCode == "39") { /* Right Arrow */
-        slider.changeSlide(1, 1);
-    }
-    if (e.keyCode == "32" && run == 1) {
-        slider.stopProgressBar(0);
-    } else if (e.keyCode == "32" && run == 0) {
-        slider.startProgressBar(1);
-    }
-
-});
 
