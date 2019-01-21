c19d28e243642edaca1099bb7fdd070e47480b18
unknown <sabouretmaxime@gmail.com>
2019-01-21 15:12:15 +0100

    starting to implement map.js

--- js/jcdecaux.js
+++
@@ -1,8 +0,0 @@
-ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=creteil&apiKey=1f9c43ee6a3f921ba77e7aa991c9dcd9a957a85f", function (response) {
-    var creteilStations = JSON.parse(response);
-    /* Display all the creteil station names */
-    creteilStations.forEach(function(station){
-        console.log(station.name);
-        console.log(station.address);
-    });
-});
---
+++ js/map.js
@@ -0,0 +1,29 @@
+
+// JCDECAUX, the second parameter is the callback function
+ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=creteil&apiKey=1f9c43ee6a3f921ba77e7aa991c9dcd9a957a85f", function (response) {
+    var creteilStations = JSON.parse(response);
+    /* Display all the creteil station names */
+    creteilStations.forEach(function(station){
+        console.log(station.name);
+        console.log(station.address);
+    });
+});
+
+// Ajax call
+function ajaxGet(url, callback) {
+    // HTTP Request
+    let req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // send the response to the callback method
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
